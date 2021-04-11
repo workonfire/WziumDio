0c7a4a0fec736f0c5b76243d6cac4e9d042126b5
workonfire <kolucki62@gmail.com>
2021-04-11 21:36:54 +0200

    Fonts cleanup

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
             echo isset($full_radio_name) ? "- " . $full_radio_name : '';
         ?></title>
     <link rel="icon" type="image/png" href="assets/icon.png"/>
+    <link rel="stylesheet" href="fonts.css">
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
